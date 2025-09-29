067355ebe13ac455835f7e0e3eaea67cb0e5610e
xuwei-k <6b656e6a69@gmail.com>
2025-09-29 11:09:13 +0900

    update scalafmt setting

--- src/main/scala/jp/co/septeni_original/sbt/dao/generator/SbtDaoGeneratorPlugin.scala
+++ src/main/scala/jp/co/septeni_original/sbt/dao/generator/SbtDaoGeneratorPlugin.scala
@@ -1,6 +1,7 @@
 package jp.co.septeni_original.sbt.dao.generator
 
 import jp.co.septeni_original.sbt.dao.generator.model.{ ColumnDesc, TableDesc }
+import org.scalafmt.interfaces.{ Scalafmt, ScalafmtSession }
 import sbt.Keys._
 import sbt.{ *, given }
 import sbt.plugins.JvmPlugin
@@ -17,6 +18,41 @@ object SbtDaoGeneratorPlugin extends AutoPlugin with SbtDaoGeneratorCompat {
   object autoImport extends SbtDaoGeneratorKeys
 
   import SbtDaoGeneratorKeys._
+
+  @transient
+  private[generator] val daoGeneratorScalafmtInstance =
+    taskKey[Option[ScalafmtSession]]("").withRank(KeyRanks.Invisible)
+
+  // https://github.com/scalameta/scalafmt/blob/b78a999c191d5afc955/scalafmt-dynamic/jvm/src/main/scala/org/scalafmt/dynamic/ConsoleScalafmtReporter.scala
+  private class MyScalafmtReporter(log: Logger) extends org.scalafmt.interfaces.ScalafmtReporter {
+    def downloadOutputStreamWriter(): java.io.OutputStreamWriter =
+      new java.io.OutputStreamWriter(scala.Console.out)
+    def downloadWriter(): java.io.PrintWriter =
+      new java.io.PrintWriter(scala.Console.out)
+    def error(file: java.nio.file.Path, message: String): Unit =
+      log.error(s"error: ${file}: ${message}")
+    def error(file: java.nio.file.Path, e: Throwable): Unit = {
+      log.error(s"error: ${file}: ${e}")
+      e.printStackTrace()
+    }
+    def excluded(filename: java.nio.file.Path): Unit =
+      log.info(s"file excluded: $filename")
+    def parsedConfig(config: java.nio.file.Path, scalafmtVersion: String): Unit =
+      log.debug(s"parsed scalafmt config (v$scalafmtVersion): $config")
+  }
+
+  override lazy val buildSettings: Seq[Setting[?]] = Def.settings(
+    daoGeneratorScalafmtInstance := {
+      val log = streams.value.log
+      // https://github.com/scalameta/sbt-scalafmt/blob/e59fc02237374e6/plugin/src/main/scala/org/scalafmt/sbt/ScalafmtPlugin.scala#L42-L45
+      TaskKey[File]("scalafmtConfig").?.value.filter(_.isFile).map { conf =>
+        Scalafmt
+          .create(this.getClass.getClassLoader)
+          .withReporter(new MyScalafmtReporter(log))
+          .createSession(conf.toPath)
+      }
+    }
+  )
 
   override def projectSettings: Seq[Def.Setting[?]] = Seq(
     generator / daoGeneratorScalafmt := true,
